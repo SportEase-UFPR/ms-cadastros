9d257ea8ff83e7e930bf353b9886f4a7f1027574
Rafael Dias Simeoni <rafaelsimeoni28@gmail.com>
2023-11-27 20:59:14 -0300

    insert inicial do adm

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,8 @@
-INSERT IGNORE INTO cadastros.tb_administradores (id, cpf, email, id_usuario, nome) VALUES (1, '00000000000' ,'adm@gmail.com', 1, 'adm');
+INSERT INTO cadastros.tb_administradores (id, cpf, email, id_usuario, nome)
+SELECT 1, '00000000000', 'adm@gmail.com', 1, 'adm'
+FROM dual
+WHERE NOT EXISTS (
+    SELECT 1
+    FROM cadastros.tb_administradores
+    WHERE id = 1
+);
